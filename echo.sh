034208e35b22be91ce1e4d83f619b23f7b93d32a
amaoworks <amaoworks@gmail.com>
2017-12-21 20:47:39 +0800

    2017/12/21

---
+++ echo.sh
@@ -0,0 +1,13 @@
+#########################################################################
+# File Name: echo.sh
+# Author: Lucio
+# mail: amaoworks@gmail.com
+# Created Time: 2017年12月21日 星期四 20时35分39秒
+#########################################################################
+#!/bin/bash
+#fileName : echo.sh
+echo -e "1\t2\t3"
+printf "\n"
+echo -e "\e[1;31m This is red text \e[0m"
+printf "\n"
+echo -e "\e[1;42m Green Background \e[0m"
---
+++ printf.sh
@@ -0,0 +1,13 @@
+#########################################################################
+# File Name: printf.sh
+# Author: Lucio
+# mail: amaoworks@gmail.com
+# Created Time: 2017年12月21日 星期四 20时25分38秒
+#########################################################################
+#!/bin/bash
+#fileName : printf.sh
+
+printf "%-5s %-10s %-4s\n" No Name  Mark
+printf "%-5s %-10s %-4.2f\n" 1 Sarath 80.3456
+printf "%-5s %-10s %-4.2f\n" 2 James 90.9989
+printf "%-5s %-10s %-4.2f\n" 3 Jeff 77.564
