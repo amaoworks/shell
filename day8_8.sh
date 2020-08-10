56c06e3b75a9dee5810020de2af68afbfe05c0bd
maoyiyao <maoyiyao@sagereal.com>
2020-08-10 11:35:13 +0800

    Company / add some thing before

--- day8_8.sh
+++ day8_8.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+:<<!
 echo "Shell -> -> -> "
 echo "File name : $0"
 echo "First  : $1"
 echo "Second : $2"
 echo "Third  : $3"
+!
+
+myUrl="www.google.com"
+readonly myUrl
+myUrl="www.baidu.com"
